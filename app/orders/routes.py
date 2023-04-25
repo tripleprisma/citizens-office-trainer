c597e2e1f470819110d76adc15a38c8baa2e9b75
leonard <leonard@musekit.io>
2023-04-25 20:46:21 +0200

    add CRUD functionality

--- app/orders/routes.py
+++ app/orders/routes.py
@@ -1,32 +1,25 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from app.cookies.models import Cookie
-from app.orders.models import Order
+from app.orders.models import Order, Address
 
 blueprint = Blueprint('orders', __name__)
 
-# @blueprint.post('/checkout')
-# def post_checkout():
-#   try:
-#     cookies = Cookie.query.all()
 
-#     if not all([
-#       request.form.get('name'),
-#       request.form.get('street'),
-#       request.form.get('city'),
-#       request.form.get('state'),
-#       request.form.get('zip'),
-#       request.form.get('country')
-#     ]):
-#       raise Exception('Please fill out all address fields.')
+@blueprint.route('/checkout')
+def checkout():
+    return render_template('orders/new.html')
 
-#     create_order(request.form, cookies)
-#     return render_template('orders/new.html', cookies=cookies)
-#   except Exception as error_message:
-#     error = error_message or 'An error occurred while processing your order. Please make sure to enter valid data.'
 
-#     current_app.logger.info(f'Error creating an order: {error}')
+@blueprint.post('/checkout')
+def post_checkout():
 
-#     return render_template('orders/new.html', 
-#       cookies=cookies,
-#       error=error
-#     )
+    address = Address(
+        name=request.form.get('name'),
+        street=request.form.get('street'),
+        city=request.form.get('city'),
+        state=request.form.get('state'),
+        zip=request.form.get('zip'),
+        country=request.form.get('country')
+    )
+    address.save()
+    return redirect(url_for('cookies.cookies'))
